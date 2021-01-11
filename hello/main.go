01236a384613041c66fa0a3e366e0a57d8e38ab9
Antoine FERRO <antoine.ferro@supdevinci-edu.fr>
2021-01-11 13:54:59 +0100

    ajout flag version

--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
+const VERSION = "1.0"
+
 func main() {
+	var version = flag.Bool("version", true, "Version prog")
 	fmt.Println("Hello World")
+	if *version == true {
+		fmt.Println(VERSION)
+	}
 }
